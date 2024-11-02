bd978d3672dbacc6c954db00ff228a15f7211e38
Dev-SSS1267 <sams3s1267@gmail.com>
2024-11-02 15:56:38 +0900

    Refactor HomeView.vue to add login redirection

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,8 @@
     <img src="https://cdn.lunaiz.com/my_uni_assets/logo-l.svg" alt="my uni" class="w-40 mx-auto mb-2">
     <p class="text-lg text-gray-600 mt-2">내가 지원한 대학과 결과를 공유해보세요</p>
     <button
-      class="mt-[250px] rounded-lg bg-gradient-to-br from-blue-500 to-purple-500 py-2 w-[90%] text-lg text-white font-semibold">로그인</button>
+      class="mt-[250px] rounded-lg bg-gradient-to-br from-blue-500 to-purple-500 py-2 w-[90%] text-lg text-white font-semibold"
+      onClick="location.href='/my';">로그인</button>
   </div>
 
 </template>
